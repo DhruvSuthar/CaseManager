751894bbd29e61df3ff62a81836611d821dcd501
DhruvSuthar <dhruvsuthar17@live.com>
2017-04-17 22:56:37 +0530

    Added DataStructures

---
+++ CaseManager/Modules/DataStructures/History.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseManager.Modules.DataStructures
+{
+    public class History
+    {
+        public string BloodPressure { get; set; }
+        public float Temperature { get; set; }
+        public string HistoryText { get; set; }
+        public string Treatment { get; set; }
+    }
+}
---
+++ CaseManager/Modules/DataStructures/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseManager.Modules.DataStructures
+{
+    public class Patient
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public DateTime CaseDate { get; set; }
+        public DateTime ExpDate { get; set; }
+        public List<History> HistoryData { get; set; }
+        public float CashTaken { get; set; }
+    }
+}
