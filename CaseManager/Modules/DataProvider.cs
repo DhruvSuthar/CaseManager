96f13bb8613d842d89b8b82f66fce4ac79e6958b
DhruvSuthar <dhruvsuthar17@live.com>
2017-04-17 22:26:48 +0530

    Added DataProvider and Strings

---
+++ CaseManager/Modules/DataProvider.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CaseManager.Modules
+{
+    public class DataProvider
+    {
+        private SqlConnection conn;
+        private SqlCommand cmd;
+
+        public DataProvider()
+        {
+            conn = new SqlConnection(Strings.ConnectionString);
+            conn.Open();
+            cmd = new SqlCommand("", conn);
+        }
+
+        public async void Insert(List<string> data, string Table)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    cmd.CommandText = "INSERT INTO " + Table + " VALUES('";
+                    int i = 0;
+                    foreach (var item in data)
+                    {
+                        i++;
+                        cmd.CommandText += item;
+                        if (i == data.Count) cmd.CommandText += "')";
+                        else cmd.CommandText += "', '";
+                    }
+                    cmd.ExecuteNonQuery();
+                }
+                catch(Exception)
+                {
+                    
+                }
+            }); 
+        }
+        public async Task<List<string>> SelectStarFromField(string table, string field, string where = "")
+        {
+            var ls = new List<string>();
+            await Task.Run(() =>
+            {
+                try
+                {
+                    cmd.CommandText = "SELECT " + field + " FROM " + table;
+                    if (where != "") cmd.CommandText += " where " + where;
+                    var r = cmd.ExecuteReader();
+                    while (r.Read())
+                    {
+                        ls.Add(r.GetString(0));
+                    }
+                    r.Close();
+                }
+                catch(Exception) { }
+            });
+            return ls;
+        }
+        public async void DeleteFrom(string table, string where)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    cmd.CommandText = "DELETE FROM " + table + " WHERE " + where;
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception) { }
+            });
+        }
+        public async void Update(string table, string set, string where)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    cmd.CommandText = "UPPDATE " + table + " SET " + set + " WHERE " + where;
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception) { }
+            });
+        }
+        public async Task<List<List<string>>> Select(string table, string columns, string where = "")
+        {
+            var ls = new List<List<string>>();
+            await Task.Run(() =>
+            {
+                try
+                {
+                    cmd.CommandText = "SELECT " + columns + " FROM " + table;
+                    if (where != "") cmd.CommandText += " WHERE " + where;
+                    var r = cmd.ExecuteReader();
+                    int c = columns.Split(',').Length;
+                    while (r.Read())
+                    {
+                        var l = new List<string>();
+                        for (int i = 0; i < c; i++)
+                        {
+                            l.Add(r.GetString(i));
+                        }
+                        ls.Add(l);
+                    }
+                    r.Close();
+                }
+                catch (Exception) { }
+            });
+            return ls;
+        }
+    }
+}
---
+++ CaseManager/Modules/Strings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseManager.Modules
+{
+    public class Strings
+    {
+        public static string ConnectionString { get { return AppDomain.CurrentDomain.BaseDirectory; } }
+
+        //Tables
+        public static string Patient = "PatientData";
+        public static string Stats = "Statistics";
+        public static string Turnover = "Turnover";
+
+    }
+}
